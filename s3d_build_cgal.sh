be11f16ce2e733229e2acb6699c1de20dd6f7820
Francis St-Amour <francis.st-amour@shapeshift3d.com>
2021-01-27 20:42:48 +0000

    ci: add missing build script
    # Conflicts:
    #	s3d_build_cgal.sh

--- s3d_build_cgal.sh
+++ s3d_build_cgal.sh
@@ -2,9 +2,16 @@
 
 . "${S3D_SOURCES_DIR}/common.sh"
 
-readonly source_dir="${S3D_SOURCES_DIR}/cgal"
-readonly build_dir="${S3D_BUILDS_DIR}/cgal"
-readonly install_dir="${S3D_INSTALL_DIR}/cgal"
+readonly project_name=cgal
+readonly build_dir="${S3D_BUILDS_DIR}/${project_name}"
+readonly install_dir="${S3D_INSTALL_DIR}/${project_name}"
+
+# If running in Windows CI
+if [ ! -z ${CI_PROJECT_DIR+x} ] && [ ${OS} = "Windows_NT" ]; then
+    readonly source_dir="${PWD}/"
+else
+    readonly source_dir="${S3D_SOURCES_DIR}/${project_name}"
+fi
 
 # Create build directory
 mkdir -p "${build_dir}"
@@ -12,13 +19,34 @@ mkdir -p "${build_dir}"
 # configure
 cd "${build_dir}"
 # configure and generate
-cmake \
-  -G "${GENERATOR}" \
-  -DCMAKE_INSTALL_PREFIX="${install_dir}" \
-  -DCGAL_HEADER_ONLY=ON \
-  -DWITH_Eigen3=ON \
-  -DCMAKE_BUILD_TYPE=Release \
-  "${source_dir}"
+case $OS in
+    linux)
+	cmake \
+	    -G "${GENERATOR}" \
+	    -DCMAKE_INSTALL_PREFIX="${install_dir}" \
+	    -DCGAL_HEADER_ONLY=ON \
+	    -DWITH_Eigen3=ON \
+	    -DCMAKE_BUILD_TYPE=Release \
+	    "${source_dir}"
+	;;
+    Windows_NT)
+	readonly mpfr_install_dir="${S3D_INSTALL_DIR}/mpfr"
+	cmake \
+	    -G "${GENERATOR}" \
+	    -DCMAKE_INSTALL_PREFIX="${install_dir}" \
+	    -DCGAL_HEADER_ONLY=ON \
+	    -DWITH_Eigen3=ON \
+	    -DCMAKE_BUILD_TYPE=Release \
+	    `# BOOST ` \
+	    -DBOOST_ROOT="${S3D_INSTALL_DIR}/boost" \
+	    `# GMP ` \
+	    -DGMP_INCLUDE_DIR="${mpfr_install_dir}/mpir/dll/x64/Release" \
+	    -DGMP_LIBRARIES="${mpfr_install_dir}/mpir/dll/x64/Release/mpir.lib" \
+	    -DMPFR_INCLUDE_DIR="${mpfr_install_dir}/mpfr" \
+	    -DMPFR_LIBRARIES="${mpfr_install_dir}/mpfr/dll/x64/Release/mpfr.lib" \
+	    "${source_dir}"
+	;;
+esac
 
 build
 install
